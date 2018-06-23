fdf81adf0dd281f46ad8d195efead354e1bdafc4
TimAndy <xuchonglei@126.com>
2018-06-23 09:33:50 +0800

    修改为常驻后台并且只能启动一个进程

--- VdiCrack/VdiCrack.cpp
+++ VdiCrack/VdiCrack.cpp
@@ -3,18 +3,30 @@
 
 #include "stdafx.h"
 #include "VdiCrack.h"
-#include <WinUser.h>
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow)
+#define INTERVAL            10000
+#define MUTEXT_NAME         "VDI_CRACK"
+#define CLASS_WATER_MARK    "waterMark_wclass"
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
-	HWND hwndWaterMark = FindWindow(_T("waterMark_wclass"), NULL);
-	if (hwndWaterMark == 0)
-		return 0;
+    HANDLE hMutex = CreateMutex(NULL, false, _T(MUTEXT_NAME));
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+        return 0;
 
-	SetLayeredWindowAttributes(hwndWaterMark, 0, 0, 0);
-	SetWindowPos(hwndWaterMark, HWND_BOTTOM, -100000, -100000, 0, 0, SWP_ASYNCWINDOWPOS);
-	return 0;
+    HWND hwndWaterMarkLast = HWND_DESKTOP;
+    do {
+        HWND hwndWaterMark = FindWindow(_T(CLASS_WATER_MARK), NULL);
+        if (hwndWaterMark == 0 || hwndWaterMark == hwndWaterMarkLast) {
+            hwndWaterMarkLast = hwndWaterMark;
+            Sleep(INTERVAL);
+            continue;
+        }
+        SetLayeredWindowAttributes(hwndWaterMark, 0, 0, 0);
+        SetWindowPos(hwndWaterMark, HWND_BOTTOM, -100000, -100000, 0, 0, SWP_ASYNCWINDOWPOS);
+        hwndWaterMarkLast = hwndWaterMark;
+        Sleep(INTERVAL);
+    } while (true);
+
+    ReleaseMutex(hMutex);
 }
