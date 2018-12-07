a9c70ff482d2eb4fe1dcdb95cf23e4561e6fe895
TimAndy <xuchonglei@126.com>
2018-12-07 16:29:15 +0800

    安装程序提取配置文件

---
+++ VdiCrackSetup/Config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define MUTEXT_RUN          _T("VDI_CRACK_RUN")
+#define MUTEXT_SHUTDOWN     _T("VDI_CRACK_SHUTDOWN")
+#define EXE_OPERATION       _T("open")
+#define EXE_PATH            _T("C:\\Program Files\\VdiCrack\\VdiCrack.exe")
+#define REG_NAME            _T("VdiCrack")
+#define REG_RUN             _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run")
+#define WAIT_INTERVAL       1000
--- VdiCrackSetup/stdafx.h
+++ VdiCrackSetup/stdafx.h
@@ -20,3 +20,4 @@
 
 
 // TODO: 在此处引用程序需要的其他头文件
+#include "Config.h"
